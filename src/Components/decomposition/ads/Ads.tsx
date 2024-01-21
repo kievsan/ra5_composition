37f1cf5a01cf741b39bdf7821c9bd187dd5f858a
kievskiy <knhel7@mail.ru>
2024-01-22 01:28:17 +0300

    News block

--- src/Components/decomposition/ads/Ads.tsx
+++ src/Components/decomposition/ads/Ads.tsx
@@ -1,6 +1,3 @@
-import Card from '../Card';
-import {btn} from '../Card'
-
 import classes from '../css/main.module.css';
 
 /**
@@ -9,8 +6,9 @@ import classes from '../css/main.module.css';
 export default function Ads() {
   return (
     <div className={classes['ads']}>
-      <Card img='https://via.placeholder.com/180x140?text=Image' button={btn('Работа над ошибками', '#')} />
-      <p className={classes['ads-content']}>Смотрите на Яндексе и запоминайте</p>
+        <img src="https://via.placeholder.com/180x140?text=Image" alt="ads"/><br />
+        <a href="#">Работа над ошибками</a>
+        <p className={classes['ads-content']}>Смотрите на Яндексе и запоминайте</p>
     </div>
   );
 };
